9603354859da2ea397e97c1e709c1c4faaee5b22
Nick Matteo <kundor@kundor.org>
2016-12-15 09:57:12 -0700

    Add some docstrings

--- findfirst.py
+++ findfirst.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
 Created on Fri Dec  9 09:20:28 2016
@@ -9,6 +8,7 @@ from numba import jit
 
 @jit(nopython=True)
 def findfirstgt(vec, val):
+    """Find index of first value in vec greater than val, or -1 if none exist."""
     for i in range(len(vec)):
         if vec[i] > val:
             return i
@@ -16,6 +16,7 @@ def findfirstgt(vec, val):
 
 @jit(nopython=True)
 def findfirstgte(vec, val):
+    """Index of first value in vec greater than or equal to val, or -1 if none exist."""
     for i in range(len(vec)):
         if vec[i] >= val:
             return i
@@ -23,6 +24,7 @@ def findfirstgte(vec, val):
 
 @jit(nopython=True)
 def findfirstlt(vec, val):
+    """Find index of first value in vec less than val, or -1 if none exist."""
     for i in range(len(vec)):
         if vec[i] < val:
             return i
@@ -30,12 +32,20 @@ def findfirstlt(vec, val):
 
 @jit(nopython=True)
 def findfirstlte(vec, val):
+    """Index of first value in vec less than or equal to val, or -1 if none exist."""
     for i in range(len(vec)):
         if vec[i] <= val:
             return i
     return -1
 
 def makefindpred(pred):
+    """Create a JIT function to find first value satisfying a given predicate.
+
+    The returned function takes an array and returns the index of the first value
+    satisfying pred, which must itself be a JIT function.
+    """
+# This workaround is necessary because nopython JIT functions cannot take functions
+# as arguments.
     @jit(nopython=True)
     def findfirstpred(vec):
         for i in range(len(vec)):
