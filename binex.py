362ae55e9943a070163305c9d6b6bebe29f736dd
Nick Matteo <kundor@kundor.org>
2017-03-10 16:54:54 -0700

    Default to console logging output

--- binex.py
+++ binex.py
@@ -6,10 +6,11 @@ from functools import reduce
 from operator import xor
 from hashlib import md5
 from binascii import crc32, crc_hqx
+import sys
 
 SYNC = b'\xC2'
 
-def info(*args, logfile=open('/tmp/loggin', 'a')):
+def info(*args, logfile=sys.stderr):
     print(*args, file=logfile, flush=True)
 
 def readsync(strm):
