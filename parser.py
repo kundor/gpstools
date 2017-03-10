a30ce8f90756d94ebb8feac5c1bfb47599443bb3
Nick Matteo <kundor@kundor.org>
2017-03-10 16:54:32 -0700

    Numpy data formats

--- parser.py
+++ parser.py
@@ -5,5 +5,13 @@ Created on Fri Mar 10 09:04:30 2017
 
 @author: nima9589
 """
+from binex import *
+import numpy as np
 
 IN = open(0, 'rb')
+
+SNR_REC = np.dtype([('prn', 'u1'), ('time', 'M8[us]'), ('el', 'f'), ('az', 'f'), ('snr', 'u2')])
+HK_REC = np.dtype([('rxid', 'u1'), ('time', 'M8[us]'), ('mac', 'u8'), ('lon', 'i4'),
+                   ('lat', 'i4'), ('alt', 'i4'), ('volt', 'u2'), ('temp', 'i2'),
+                   ('msgct', 'u2'), ('err', 'u1')])
+
