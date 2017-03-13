b5944e34fc424d194c76784a868ab7a1a80c890f
Kolya Matteo <kundor@kundor.org>
2017-03-12 21:14:14 -0600

    Add snr88 and housekeeping output.

--- parser.py
+++ parser.py
@@ -6,6 +6,7 @@ Created on Fri Mar 10 09:04:30 2017
 @author: nima9589
 """
 from collections import defaultdict
+import sys
 import numpy as np
 from binex import read_record, info
 from ascii_read import gpstotsecgps, poslist
@@ -50,6 +51,10 @@ def weeksow_to_np(week, sow):
     """Convert GPS week and (fractional) second-of-week to numpy datetime64 in GPS time."""
     return GPSepoch + np.timedelta64(week, 'W') + np.timedelta64(round(sow*1e6), 'us')
 
+def sod(time):
+    """Convert numpy datetime64 to seconds of day."""
+    return (time - time.astype('M8[D]')) / np.timedelta64(1, 's')
+
 def assignrec(arr, ind, val):
     """Assign val to arr[ind], growing it if necessary."""
     try:
@@ -70,11 +75,11 @@ def addrecords(SNR, time, snrs, curi, cofns, rxloc, trans):
     return curi
 
 def readall(fid):
-    SNRs = defaultdict(lambda: np.empty(1000, dtype=SNR_REC))
+    SNRs = defaultdict(lambda: np.recarray((1000,), dtype=SNR_REC))
     curind = defaultdict(int) # how far we've filled the corresponding SNR array
     rxloc = {} # for each rxid, its location, filled in when we get a HK record
     trans = {} # for each rxid, a ENU translation matrix, filled in when location known
-    HK = np.empty(1000, dtype=HK_REC)
+    HK = np.recarray((1000,), dtype=HK_REC)
     curh = 0
     cofns = None # compute as soon as we find a good time record
     numempty = numearly = numnoloc = 0
@@ -126,3 +131,58 @@ def readall(fid):
     for rxid in curind:
         SNRs[rxid].resize((curind[rxid],))
     return SNRs, HK
+
+
+def out_snr88(SNR, filename):
+    """Output the records in the numpy array SNR to filename in snr88 format."""
+    with open(filename, 'wt') as fid:
+        for rec in SNR:
+            fid.write('{} {:.4f} {:.4f} {:.1f} 0 0 {:.1f} 0 0'.format(
+                       rec.prn, rec.el, rec.az, sod(rec.time), rec.snr/10)) 
+
+def write_all_snr88(SNRs, prefix='rx'):
+    """Write out each receiver's snr records in a file in the current directory.
+
+    Filename is rx##DOY0.YY.snr88. This assumes that all the data is from the same
+    GPS day.
+    """
+    for rxid, SNR in SNRs.items():
+        epoch = SNR[0].time
+        fname = prefix + '{:02}'.format(rxid) + epoch.tolist().strftime('%j0.%y.snr88')
+        out_snr88(SNR, fname)
+
+def macformat(mac):
+    hs = '{:016X}'.format(mac)
+    return ':'.join(hs[i:i+4] for i in range(0, len(hs), 4))
+
+def hkreport(HK, file=None):
+    """Print out the contents of housekeeping messages in the array HK."""
+    for rec in HK:
+        print('Rx{0.rxid:02} {1:%x %X}  {2}  {3:.4f}ºE {4:.4f}ºN {5:.3f}m  {6:.2f}V {0.temp:2}ºC msgct:{0.msgct:5}  flags: {0.err:02X}'.format(rec, rec.time.tolist(), macformat(rec.mac), rec.lon/1e7, rec.lat/1e7, rec.alt/1000, rec.volt/100), file=file)
+
+def translate(fid):
+    """Translate BINEX data to ASCII formats.
+
+    Read in data from the provided file object, and write out snr88 files
+    and a housekeeping report in the current directory.
+    """
+    SNRs, HK = readall(fid)
+    write_all_snr88(SNRs)
+    epoch = HK[0].time
+    hkfile = epoch.tolist().strftime('HK_%j.%y.txt') # we need a site identifier
+    with open(hkfile, 'wt') as fid:
+        hkreport(HK, fid)
+
+# To read from stdin:
+# translate(open(0, 'rb'))
+
+if __name__ == "__main__": # When this file is run as a script
+    if len(sys.argv) != 2:
+        print("Usage:", sys.argv[0], "<file>\n"
+              "<file> should be the path to VAPR BINEX data. "
+              "snr88 files and a housekeeping report will be written out "
+              "in the current directory.")
+        sys.exit(1)
+    with open(sys.argv[1], 'rb') as fid:
+        translate(fid)
+
