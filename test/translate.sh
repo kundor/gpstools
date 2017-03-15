9c82725688b0432c478ca06869de15f68ef844c6
Nick Matteo <kundor@kundor.org>
2017-03-14 19:51:31 -0600

    Fix rm command

--- test/translate.sh
+++ test/translate.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-rm rx050740.17.snr88, rx060740.17.snr88, HK_074.17.txt
+rm rx050740.17.snr88 rx060740.17.snr88 HK_074.17.txt
 ../parser.py data.bin
 diff rx050740.17.snr88 rx050740.17.snr88.GOOD
 diff rx060740.17.snr88 rx060740.17.snr88.GOOD
