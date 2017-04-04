0bdbff052f74eeec9cdebbaf2bc5cc1ed92b2a2c
Nick Matteo <kundor@kundor.org>
2017-04-04 17:13:24 -0600

    properly span two rows

--- plot.py
+++ plot.py
@@ -285,7 +285,7 @@ def prn_snr(SNR, rxid=None, hrs=None, endtime=None, omit_zero=True, minelev=0, f
             opsnr = OSNR[OSNR['prn'] == prn]
             dax, eax = plot_old_snr(fig, gs, i, ax, psnr, opsnr, diftime, prev, dax, eax)
         if doazel:
-            ax1 = fig.add_subplot(gs[2*i:2*i + 1, 1], projection='polar')
+            ax1 = fig.add_subplot(gs[2*i:2*i + 2, 1], projection='polar')
             polarazel(psnr['az'], psnr['el'], ax1, label_el=False)
     axes[-1].set_xlabel('Time (UTC)')
     axes[0].set_ylim(minsnr, maxsnr)
