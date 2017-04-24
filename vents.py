0945f017ff2de9405f9a0a8ac1a86061d06e9ef9
Nick Matteo <kundor@kundor.org>
2017-04-24 13:49:17 -0600

    File with vent locations

---
+++ vents.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Vent locations by site.
+
+Created on Mon Apr 24 13:47:32 2017
+
+@author: nima9589
+"""
+# This is practially JSON
+VENTS = {
+        'VB001': [{ "lng": -105.26175, "lat": 40.0076 }]
+        }
